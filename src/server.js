f4a41786dae9a5e485873e46ec399cbcbfe5c263
Sidney.Li <sidneyli@augmentum.com.cn>
2025-02-10 15:48:43 +0800

    init

---
+++ src/server.js
@@ -0,0 +1,21 @@
+// ESM
+import Fastify from 'fastify'
+// import dbConnector from './services/db.js'
+import routers from './routers/index.js'
+
+/**
+ * @type {import('fastify').FastifyInstance} Instance of Fastify
+ */
+const fastify = Fastify({
+  logger: true
+})
+// fastify.register(dbConnector)
+fastify.register(routers)
+
+fastify.listen({ port: 3000 }, function (err, address) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+  // Server is now listening on ${address}
+})
